4738f912b45340b9fee51e01557032b503933c27
JeongWonLim <cdnljw@gmail.com>
2022-11-14 19:09:02 +0900

    MESSAGE 작성

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,17 +3,51 @@
 
 SECTION .TEXT 	; TEXT 세그먼트 정의
 
-MOV ax, 0xB800
-MOV ds, ax 	; DS 세그먼트 레지스터에 비디오 제어 주소를 복사
+jmp 0x07C0:START 	; CS=0x07C0, IP(PC)=0x5(START의 인덱스)
 
-MOV byte[0x00], 'M'	; DS : PC(IP) = 0xB8000:0, 세그먼트 주소는 0x4만큼 곱해준다.
-MOV byte[0x01], '0x4A'	; 빨간배경 밝은 녹색
+START:				; 0x5
+	mov ax, 0x07C0
+	mov ds, ax		;ds 레지스터를 파일이 로드되어있는 메모리 첫 주소
+	mov ax, 0xB800	;비디오 메모리 시작 주소
+	mov es, ax		;es=0xB800
+	
+	mov si, 0		;SI=0 원본 인덱스 레지스터
 
-JMP $
+.SCREENCLEARLOOP:	;clear monitor
+	mov byte [es:si], 0 	; 비디오 메모리의 0을 넣을 경우 클리어됨
+	mov byte [es:si + 1], 0x0A 		;바탕색=밝은녹색
 
-TIMES 510 - ($ -$$) DB 0x00 ; $ 현재 라인 주소
+	add si, 2		; 2byte 인덱스
+
+	cmp si, 80 * 25 * 2	;화면전체크기= 80*25, 문자인식 = 1, 색조정=1
+						;si 와 일치하지 않으면 Zero register = 1
+	jl .SCREENCLEARLOOP ; 
+
+	mov si, 0		;si 레지스터 초기화
+	mov di, 0		;di 레지스터 초기화
+
+.MESSAGELOOP:		;메세지 출력 함수
+	mov cl, byte [MESSAGE1 + si]	;CX레지스터 하위 8비트에 
+									;MESSAGE1에 SI OFFSET
+
+	cmp cl, 0
+	je .MESSAGEEND
+
+	mov byte [es:di], cl
+
+	add si, 1
+	add di, 2
+
+	jmp .MESSAGELOOP
+
+.MESSAGEEND:
+	jmp $
+
+MESSAGE1:	db 'MINT64 OS Boot Loader Start~!!', 0 ;
+
+times 510 - ($ -$$) db 0x00 ; $ 현재 라인 주소
 			; $$ 현재 섹션(.TEXT)의 시작주소
 			; $-$$ 현재 섹션을 기준으로 하는 오프셋
 
-DB 0x55
-DB 0xAA ; magic number 0x55AA 부트로더임을 표시
+db 0x55
+db 0xAA ; magic number 0x55AA 부트로더임을 표시
